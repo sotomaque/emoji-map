d5bde544d596e8d407fcff8036809803c42e626c
Enrique <sotomaque@gmail.com>
2025-03-02 15:35:44 -0800

    Initial Commit

---
+++ emoji-map/emoji_mapApp.swift
@@ -0,0 +1,17 @@
+//
+//  emoji_mapApp.swift
+//  emoji-map
+//
+//  Created by Enrique on 3/2/25.
+//
+
+import SwiftUI
+
+@main
+struct emoji_mapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ emoji-mapTests/emoji_mapTests.swift
@@ -0,0 +1,17 @@
+//
+//  emoji_mapTests.swift
+//  emoji-mapTests
+//
+//  Created by Enrique on 3/2/25.
+//
+
+import Testing
+@testable import emoji_map
+
+struct emoji_mapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
